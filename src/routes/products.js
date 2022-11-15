2df9f405b169a42d57636bf419fc74193d1320bb
i-corag <icoraglio@gmail.com>
2022-11-15 19:57:37 +0100

    pages added

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,36 +4,37 @@ const path = require ("path");
 
 const router = express.Router();
 
+
 const products = [
     {
         id:"1",
         titulo:"Feminismo Para Principiantes",
         autor: "Nuria Varela",
-        imagen: "../../public/assets/libros/5.jpeg",
+        imagen: "/assets/libros/1.jpeg",
     },
     {
         id:"2",
         titulo:"El Chico de las Estrellas",
         autor: "Chris Pueyo",
-        imagen:"../../public/assets/libros/2.jpeg",
+        imagen: "/assets/libros/2.jpeg",
     },
     {
         id:"3",
         titulo:"Ecologismo Real",
         autor: "J. M. Mulet",
-        imagen:"../../public/assets/libros/3.jpeg",
+        imagen:"/assets/libros/3.jpeg",
     },
     {
         id:"4",
         titulo:"Puta Madre",
         autor: "Dalia F. Walker",
-        imagen: "../../public/assets/libros/4.jpeg",
+        imagen: "/assets/libros/4.jpeg",
     },
     {
         id:"5",
         titulo:"Los hombres me explican cosas",
         autor: "Rebecca Solnit",
-        imagen:"../../public/assets/libros/5.jpeg",
+        imagen:"/assets/libros/5.jpeg",
     },
 ];
 
